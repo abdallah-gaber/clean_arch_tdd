d8724b32629295d71e8b9ca2e7d3d29bf8b8d516
AbdallahGaber <agaber@is.net.sa>
2021-07-11 16:04:57 +0200

    writing the domain and data layers with it's test

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,16 @@
+import 'package:clean_arch_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter/material.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({@required String text, @required int number})
+      : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> jsonMap) {
+    return NumberTriviaModel(
+        text: jsonMap['text'], number: (jsonMap['number'] as num).toInt());
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'text': text, 'number': number};
+  }
+}
